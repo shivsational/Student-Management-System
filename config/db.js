94056635714e3954552ee2aea92845a3b9881169
Shiv Yadav <shiv4yadav123@gmail.com>
2026-07-10 02:33:36 +0530

    Update Railway database configuration

--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@ const mysql = require("mysql2");
 
 // Create Database Connection
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+host: process.env.MYSQLHOST,
+port: process.env.MYSQLPORT,
+user: process.env.MYSQLUSER,
+password: process.env.MYSQLPASSWORD,
+database: process.env.MYSQLDATABASE
 });
 
 // Connect to MySQL
